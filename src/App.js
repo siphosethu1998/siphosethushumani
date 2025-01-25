b659c01553fcc1e61d685315a024baf0beaba482
siphosethu1998 <siphosethu1998@gmail.com>
2025-01-25 16:18:13 +0200

    finished styling hero section

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="bg-gray-50 font-nunito h-screen ">
+    <div className="bg-gray-50 flex flex-col font-nunito h-screen">
       <NavBar />
+      <main className="px-80">
+        {/* hero section */}
+        <section className="flex gap-24 items-center">
+          <div className="flex flex-col gap-6">
+            <h1 className="text-4xl font-bold text-bright-blue"><span className="text-gray-700">Hi, I'm</span> Siphosethu Shumani</h1>
+            <p className="w-4/5 text-gray-500">A passionate Full-Stack Developer committed to building efficient and scalable web applications.</p>
+            <a href="#contact" className="bg-bright-blue text-white w-fit py-3 px-6 rounded-full">Get in touch</a>
+          </div>
+          <img src="/my_image.jpg" alt="profile-pic" className="size-72 rounded-full shadow-md"/>
+        </section>
+      </main>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,16 @@
 const NavBar = () => {
+  const navStyling = "hover:text-bright-blue transition-colors";
+
   return (
-    <div className="flex justify-around py-4 bg-white items-center shadow-md">
+    <div className="flex space-x-[550px] py-4 bg-white justify-center items-center shadow-md mb-44">
       <a href="#header" className="text-2xl font-bold text-bright-blue">Siphosethu Shumani</a>
       <ul className="flex gap-8 font-semibold text-gray-700">
-        <li className="hover:text-bright-blue transition-colors"><a href="#skills">Skills</a></li>
-        <li className="hover:text-bright-blue transition-colors"><a href="#services">Services</a></li>
-        <li className="hover:text-bright-blue transition-colors"><a href="#projects">Projects</a></li>
-        <li className="hover:text-bright-blue transition-colors"><a href="#experience">Experience</a></li>
-        <li className="hover:text-bright-blue transition-colors"><a href="#education">Education</a></li>
-        <li className="hover:text-bright-blue transition-colors"><a href="#contact">Contact</a></li>
+        <li className={`${navStyling}`}><a href="#skills">Skills</a></li>
+        <li className={`${navStyling}`}><a href="#services">Services</a></li>
+        <li className={`${navStyling}`}><a href="#projects">Projects</a></li>
+        <li className={`${navStyling}`}><a href="#experience">Experience</a></li>
+        <li className={`${navStyling}`}><a href="#education">Education</a></li>
+        <li className={`${navStyling}`}><a href="#contact">Contact</a></li>
       </ul>
     </div>
   );
